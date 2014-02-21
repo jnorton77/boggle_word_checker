41b8271a4bc422a05fdeaeeaa0cb033f47816bb7
John_and_Tony <tonyta.tt@gmail.com>
2014-02-21 00:25:21 -0600

    refactored the BoggleBoard class

--- source/boggle_board.rb
+++ source/boggle_board.rb
@@ -1,29 +1,17 @@
 class BoggleBoard
   attr_accessor :board
 
-  DICE = [ 'AAEEGN',
-           'ELRTTY',
-           'AOOTTW',
-           'ABBJOO',
-           'EHRTVW',
-           'CIMOTU',
-           'DISTTY',
-           'EIOSST',
-           'DELRVY',
-           'ACHOPS',
-           'HIMNQU',
-           'EEINSU',
-           'EEGHNW',
-           'AFFKPS',
-           'HLNNRZ',
-           'DEILRX'  ].map(&:chars)
+  DICE = [ 'AAEEGN', 'ELRTTY', 'AOOTTW', 'ABBJOO',
+           'EHRTVW', 'CIMOTU', 'DISTTY', 'EIOSST',
+           'DELRVY', 'ACHOPS', 'HIMNQU', 'EEINSU',
+           'EEGHNW', 'AFFKPS', 'HLNNRZ', 'DEILRX' ].map(&:chars)
 
   def initialize
     @board = Array.new(4) {Array.new(4, '_')}
   end
 
   def shake!
-    self.board = DICE.shuffle.map{ |die_arr| roll(die_arr) }.each_slice(4).to_a
+    self.board = DICE.shuffle.map{ |die| roll(die) }.each_slice(4).to_a
   end
 
   def roll(die)
@@ -47,33 +35,38 @@ class BoggleBoard
   end
 
   def include?(word)
-    chars = word.upcase.chars
+    word = word.upcase.gsub(/qu/, 'q')
+    chars = word.chars
     board.each_index do |row|
       board.each_index do |col|
-        return true if check(chars, board, row, col)
+        return true if check?(chars, board, row, col)
       end
     end
     false
   end
 
-  def check(chars, board, row, col)
-    board = board.transpose.transpose
-    char = chars.first
-    remain = chars[1..-1]
+  def check?(chars, board, row, col)
     return false unless within_bounds?(row, col)
-    if char == board[row][col]
+    return false unless chars.first == board[row][col]
+    board = board.transpose.transpose # hacky deep cloning
+    remain = chars[1..-1]
+    if remain.empty?
+      true
+    else
       board[row][col] = '-'
-      return true if remain.empty?
-      return check(remain, board, row    , col + 1) || # right
-             check(remain, board, row    , col - 1) || # left
-             check(remain, board, row + 1, col    ) || # up
-             check(remain, board, row - 1, col    ) || # down
-             check(remain, board, row - 1, col - 1) || # down right
-             check(remain, board, row - 1, col + 1) || # down left
-             check(remain, board, row + 1, col - 1) || # up right
-             check(remain, board, row + 1, col + 1)    # up left
+      check_in_all_directions?(remain, board, row, col)
     end
-    false
+  end
+
+  def check_in_all_directions?(remain, board, row, col)
+      check?(remain, board, row    , col + 1) || # right
+      check?(remain, board, row    , col - 1) || # left
+      check?(remain, board, row + 1, col    ) || # up
+      check?(remain, board, row - 1, col    ) || # down
+      check?(remain, board, row - 1, col - 1) || # down right
+      check?(remain, board, row - 1, col + 1) || # down left
+      check?(remain, board, row + 1, col - 1) || # up right
+      check?(remain, board, row + 1, col + 1)    # up left
   end
 
   def within_bounds?(row, col)
